eb03a15a4eef4d582f24f4c852e972086e657087
sweetkane <kanesweet11@gmail.com>
2021-08-14 16:10:48 -0400

    add to reducer

--- frontend/src/reducers.js
+++ frontend/src/reducers.js
@@ -5,6 +5,7 @@ const INITIAL_USER = {
     "user_name": "DEFAULT",
     "is_validated": false,
     "is_loading": false,
+    "lot_number": "0123456789",
 
 }
 
@@ -13,10 +14,12 @@ const reducerUser = (state = INITIAL_USER, action = {}) => {
 		
         case 'set_login':
             return { ...state, is_logged_in: action.payload }
-        
         case 'set_validation':
             return { ...state, is_validated: action.payload }
-            
+        case 'set_loading':
+            return { ...state, is_loading: action.payload }
+        case 'set_user_name':
+            return { ...state, user_name: action.payload }
 	 	default: return state;
 	}
 };
