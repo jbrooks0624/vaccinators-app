d9ac1bdfe4135ad54bf59542c08b25e7da18d53d
khass2024 <88942689+khass2024@users.noreply.github.com>
2021-08-14 16:17:53 -0400

    Update Db_connector.py
    Now opens and checks database successfully

--- Backend/Db_connector.py
+++ Backend/Db_connector.py
@@ -1,10 +1,25 @@
 import sqlite3
 
-try:
-    conn = sqlite3.connect('VaccinatorsData.db')
-    print("Opened database successfully");
-except Exception as e:
-    print("Error during connection", str(e))
 
-conn.close()
+def check_in_database(first_name, last_name, lot_number):
+    try:
+        conn = sqlite3.connect('VaccinatorsData.db')
+        print("Opened database successfully");
+
+    except Exception as e:
+        print("Error during connection", str(e))
+
+    cur = conn.cursor()
+
+    select_query = "SELECT * FROM MOCK_DATA WHERE field1=? AND field2=? AND field3=?"
+    cur.execute(select_query, (first_name, last_name, lot_number))
+
+    db_list = cur.fetchall()
+    conn.close()
+
+    if len(db_list) == 1:
+        assert(db_list[0][0] == first_name and db_list[0][1] == last_name and db_list[0][2] == lot_number)
+        return True
+    else:
+        return False
 
