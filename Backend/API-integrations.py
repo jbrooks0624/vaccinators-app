d7b9439fd66c7f2d67e4fbe837dc9326d2c75ebd
khass2024 <88942689+khass2024@users.noreply.github.com>
2021-08-14 13:40:12 -0400

    Update API-integrations.py
    Kevin's Python
    Testing verify_lot_numbers based on tweets.json

--- Backend/API-integrations.py
+++ Backend/API-integrations.py
@@ -1,1 +1,22 @@
+import json
+
+
+# Opens json file and check if name is in file
+def verify_lot_numbers(database_json, frontend_name):
+    f = open(database_json, 'r')
+    s = f.read()
+    f.close()
+
+    obj = json.loads(s)
+    statuses = obj['statuses']
+
+    for tweet in statuses:
+        user = tweet['user']
+        name = user['name']
+        if frontend_name == name:
+            return True
+    return False
+
+
+print(verify_lot_numbers('tweets.json', "NASA"))
 
