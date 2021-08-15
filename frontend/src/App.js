f978ba72db91bd0425370ca2f16eacac706ff13c
sweetkane <kanesweet11@gmail.com>
2021-08-15 13:57:28 -0400

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import LoginScreen from './screens/loginScreen';
 import ValidatedScreen from './screens/validatedScreen';
 
 import { useDispatch, useSelector } from 'react-redux';
-import { Container } from '@material-ui/core';
+import { Container, Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import Header from './components/Header';
 const useStyles = makeStyles((theme) => ({
@@ -22,7 +22,7 @@ function App() {
   const { is_logged_in, user_name, is_validated, is_loading } = user_state;
   console.log("logged in: ", is_logged_in);
   return (
-    <Container  className={classes.root}>
+    <Grid className={classes.root}>
       <Header props={user_state} />
       {is_loading ? 
         <div>loading</div> :
@@ -31,7 +31,7 @@ function App() {
           is_validated ? 
             <ValidatedScreen props={user_state} /> :
             <UnValidated props={user_state} /> }
-    </Container>
+    </Grid>
   );
 }
 
